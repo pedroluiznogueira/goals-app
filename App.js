b60ccdcb98eae08fef62778cc5ff46455cd366a7
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-04-08 03:17:27 -0300

    stretch on the parent to affect the cross axis

--- App.js
+++ App.js
@@ -85,7 +85,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'center',
+    alignItems: 'stretch',
     backgroundColor: 'grey',
     padding: 30
   }
