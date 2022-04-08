b37accc0cf92f6e33d428ffdc30cf384d98dee81
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-04-08 02:04:40 -0300

    the View component only holds other components, can't have strings

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My goals app</Text>
     </View>
   );
 }
@@ -13,7 +11,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
