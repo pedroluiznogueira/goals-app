f8550619427d10eaa1d8d68a7657d1ec8db6f617
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-04-08 03:13:28 -0300

    flex box space around

--- App.js
+++ App.js
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
   squares: {
     flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: 'grey',
     padding: 30
