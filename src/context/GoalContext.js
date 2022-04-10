c15915f80eb6234e0381240f9680e01f505c5481
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-04-10 04:13:57 -0300

    finding all persisted goals when component is mounted using useEffect()

--- src/context/GoalContext.js
+++ src/context/GoalContext.js
@@ -1,13 +1,28 @@
-import { useState, createContext } from 'react';
+import { useState, useEffect, createContext } from 'react';
 import React from 'react';
 
 const GoalContext = createContext();
 
 export const GoalProvider = ( {children} ) => {
     const [goals, setGoals] = useState([]);
-
     const apiUrl = "http://b2bf-2804-14c-59-30f6-7475-4056-1df7-7875.ngrok.io";
 
+    useEffect(() => {
+        findGoals();
+    }, []);
+
+    const findGoals = async () => {
+        const response = await fetch(`${apiUrl}/goals`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        setGoals(data);
+    }
+    
     const deleteGoal = async (id) => {
         const response = await fetch(`${apiUrl}/goals/${id}`, {
             method: 'DELETE',
